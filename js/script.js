4108e8e779234b16c9b4c4f6df83c0621b519859
YRA <info@yra.com>
2020-12-07 14:54:18 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);
+
+
+
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть что?", "18");
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваш фамилия?', '');
+//answers[2] = prompt('Сколько лет?', '');
+
+//document.write(answers);
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`)
+
+//const user = "Ivan";
+//alert(`Привет, ${user}`);
